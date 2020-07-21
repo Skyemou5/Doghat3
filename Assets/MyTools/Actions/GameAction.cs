98a283ab145e48bed464891214af68a121c27413
ben <benjaminshurts@gmail.com>
2020-07-21 11:34:05 -0600

    project1

---
+++ Assets/MyTools/Actions/GameAction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Actions/Game Action")]
+public class GameAction : ScriptableObject
+{
+    public UnityAction<object> raise;
+    public UnityAction<Coroutine> raiseCoroutine;
+    public UnityAction raiseNoArgs;
+    
+    public void RaiseAction()
+    {
+        raiseNoArgs?.Invoke();
+    }
+
+    public void RaiseAction(Object obj)
+    {
+        raise?.Invoke(obj);
+    }
+
+    public void RaiseAction(float obj)
+    {
+        raise?.Invoke(obj);
+    }
+    
+    public void RaiseAction(int obj)
+    {
+        raise?.Invoke(obj);
+    }
+
+    public void RaiseAction(Transform obj)
+    {
+        raise?.Invoke(obj);
+    }
+
+    public void RaiseAction(Coroutine obj)
+    {
+        raiseCoroutine?.Invoke(obj);
+    }
+}
---
+++ Assets/MyTools/Character/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBehaviour : MonoBehaviour
+{
+    private CharacterController controller;
+    public CharacterPattern characterPatterns;
+    private WaitForFixedUpdate waitObj;
+    public UnityEvent awakeEvent, triggerEnterEvent, triggerExitEvent;
+    public bool CanRun { get; set; } = true;
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+        waitObj = new WaitForFixedUpdate();
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void SwapPattern(CharacterPattern pattern)
+    {
+        characterPatterns = pattern;
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(Start());
+    }
+    
+    private IEnumerator Start()
+    {
+        CanRun = true;
+        while (CanRun)
+        {
+            yield return waitObj;
+            characterPatterns.Call(controller);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+}
