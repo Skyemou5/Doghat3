ff1bbb10efba0165633ce5b85e3d859d29b6aba9
ben <benjaminshurts@gmail.com>
2020-07-21 12:44:30 -0600

    Level

---
+++ Assets/MyTools/Menu/ButtonBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public string levelname;
+
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene(levelname, LoadSceneMode.Single);
+    }
+}
